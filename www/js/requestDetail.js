a2386b156f075c24029372e20a9d5c657a7f0698
EasonZou23 <zyfallen3@gmail.com>
2014-07-31 11:14:36 -0400

    commit

---
+++ www/js/requestDetail.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	$('#detailHeader').text(selectedRequestType);
+
+	$('#submitBtn').click(function(e){
+		console.log(request);
+		//submmit request
+		
+		var postUrl = baseUrl + tenant + "/Request";
+
+		$.ajax({
+		  type: "POST",
+		  url: postUrl,
+		  data: JSON.stringify({portalRequest: request, id: token}),
+		  success: function(data){
+
+		  },
+		  dataType: "json"
+		});
+	});
+});
+
+
+/*
+$( '#requestDetailPage' ).on( 'pageinit',function(event){
+  alert( 'This page was just enhanced by jQuery Mobile!' );
+});
+*/
